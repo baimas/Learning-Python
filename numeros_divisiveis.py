b621d53b89cd66d65896f0e9136c18e929af7072
[baimas] <[wagnerwinicius@gmail.com]>
2022-03-29 18:32:11 -0300

    inclusão da quantidade de números

--- numeros_divisiveis.py
+++ numeros_divisiveis.py
@@ -4,7 +4,10 @@ Imprima a quantidade de números que satisfaça essa condição
 '''
 
 numeros = range(1000, 5001)
-
+soma = 0
 for i in numeros:
     if i % 5 == 0 and i % 3 == 0:
         print(f"{i} ," , end='')
+        soma = soma + 1
+
+print(f"\n\nHá {soma} números que satisfazem a condição de números divisiveis por 3 e 5")
