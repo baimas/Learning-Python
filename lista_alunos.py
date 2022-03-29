22f60e4803408f371cff5f076068c870c396e575
[baimas] <[wagnerwinicius@gmail.com]>
2022-03-29 17:55:02 -0300

    [Exercicio do livro Python e Django]

---
+++ lista_alunos.py
@@ -0,0 +1,23 @@
+'''
+Programa para solicitar cinco nomes de alunos e suas respectivas notas e imprimir a média do grupo.
+Usar uma lista para armazenar os nomes e outra para guardar as notas dos estudantes. 
+'''
+
+#Entrada
+alunos = []
+notas = []
+
+
+interador = 1
+
+while(interador <= 5):
+     nome = input(f"Insira o nome do aluno {interador}: ")
+     nota = float(input(f"Insira a nota do aluno(a) {nome}: "))
+     alunos.append(nome)
+     notas.append(nota)
+     interador += 1
+
+media = (sum(notas)/5)
+
+print(f"Esse são os nomes dos alunos: {alunos}")
+print(f"\nMedia do grupo: {media}")
